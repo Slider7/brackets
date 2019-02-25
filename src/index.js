6a03e0de9055d8e970e001da088a0613f15c6f7e
Kemalkhan Shlembayev <kemalsh771@gmail.com>
2019-02-25 18:33:39 +0600

    task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let st = str.slice(), arr = [];
+
+  const isAnyPairExist = (str1, a) =>{
+    for(let i = 0; i < a.length; i++) {
+      if (str1.indexOf(a[i]) >= 0) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    arr.push(bracketsConfig[i].join(''));
+  }
+  
+  while (isAnyPairExist(st, arr)) {
+    arr.forEach((pair) => {
+      st = st.replace(pair, '');
+    })
+  }
+    
+  return st.length === 0;
+  
 }
